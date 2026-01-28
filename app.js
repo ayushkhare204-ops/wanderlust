5207a06b96f30079bc34207310d9a7937c3c328d
Ayush Khare <ayushkhare204@gmail.com>
2026-01-28 23:47:29 +0530

    added local-passport and User schema with passport

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express=require("express");
 const app= express();
 const mongoose= require("mongoose");
 const Listing= require("./models/listing.js");
+const User= require("./models/user.js");
 const path=require("path");
 const ejsMate=require("ejs-mate");
 const expressError = require("./utils/expressError");
 const session =require("express-session");
 const flash = require("connect-flash");
+const passport= require("passport");
+const LocalStrategy= require("passport-local");
 
 const methodOverride= require("method-override");
 
@@ -17,8 +20,9 @@ app.use(methodOverride("_method"));
  app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"/public")))
 
-const listing=require("./routes/listing.js");
-const review=require("./routes/review.js");
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 
 
 main()
@@ -46,6 +50,14 @@ const sessionOptions={
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
   res.locals.success=req.flash("success");
   res.locals.error=req.flash("error");
@@ -60,9 +72,9 @@ app.get("/",async(req,res)=>{
 
 
 
-app.use("/listings",listing);
-app.use("/listings/:id/reviews",review);
-
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter)
 
 
 
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+    res.render("user/signup.ejs");
+})
+
+module.exports = router;
